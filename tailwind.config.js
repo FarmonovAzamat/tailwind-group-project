33bd2f37f0f17a3916001fd5b9a0adef6345f582
MansurMuhammadzoda <muhammadzodamansur7@gmail.com>
2023-04-07 17:56:03 +0500

    section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      'xs': {'max': '468px'},
+      'sm': {'max': '640px'},
+      'md': {'max': '768px'},
+      'lg': {'max': '1024px'},
+    },
     extend: {},
   },
   plugins: [],
